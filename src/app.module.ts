d4fc1f6330fb174b453a3e057d26de88d6523755
Roma <romanyushchyk2006@gmail.com>
2026-03-23 18:46:52 +0200

    feat(orders): scaffold orders REST API resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { ProductsModule } from './modules/products/products.module';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
