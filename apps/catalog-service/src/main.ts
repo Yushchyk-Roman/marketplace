26aae42150ac8a85eff5d67da1ffa59563b4c962
Roma <romanyushchyk2006@gmail.com>
2026-04-02 21:49:26 +0300

    feat(catalog): setup swagger, global validation and exception filter

--- apps/catalog-service/src/main.ts
+++ apps/catalog-service/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { CatalogServiceModule } from './catalog-service.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from '../../../libs/shared/src/common/filters/all-exceptions.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(CatalogServiceModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Catalog Service')
+    .setDescription('API для управління товарами')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.port ?? 3002);
 }
 bootstrap();
