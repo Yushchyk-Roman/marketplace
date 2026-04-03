120cbb74487517c19c03d57ee746297ca186f12f
Roma <romanyushchyk2006@gmail.com>
2026-04-03 14:22:35 +0300

    refactor(order-service): fix duplicate providers and encapsulate auth in feature module

--- apps/order-service/src/order-service.module.ts
+++ apps/order-service/src/order-service.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module';
 import { ConfigModule } from '@nestjs/config';
-import { PassportModule } from '@nestjs/passport';
-import { JwtStrategy } from '@app/shared';
-import { OrdersController } from './orders/orders.controller';
-import { OrdersService } from './orders/orders.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    OrdersModule,
-    PassportModule,
-  ],
-  controllers: [OrdersController],
-  providers: [OrdersService, JwtStrategy],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), OrdersModule],
+  controllers: [],
+  providers: [],
 })
 export class OrderServiceModule {}
--- apps/order-service/src/orders/orders.module.ts
+++ apps/order-service/src/orders/orders.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { PrismaModule } from '@app/shared';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from '@app/shared';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PassportModule],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [OrdersService, JwtStrategy],
 })
 export class OrdersModule {}
